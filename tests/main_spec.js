6b1490287ccb03636657252cf3e471d02c92767f
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2017-03-02 15:59:20 +0100

    Added tests for asynchronous callbacks

--- tests/main_spec.js
+++ tests/main_spec.js
@@ -1,5 +1,5 @@
 /*jslint browser: false*/
-/*global describe, it, expect*/
+/*global describe, it, expect, beforeEach*/
 'use strict';
 
 
@@ -64,5 +64,144 @@ describe('The module is a machine constructor which', function () {
                 }
             });
         }).not.toThrow();
+    });
+});
+
+
+describe('Machine constructor, given an object,', function () {
+    var o = {
+        states: {
+            initial: function () {
+                return this.states.final;
+            },
+            final: function () {
+                return;
+            }
+        }
+    };
+    var m;
+
+    beforeEach(function () {
+        m = u(o);
+    });
+
+    it('creates property "current"', function () {
+        expect(typeof o.current).toBe('function');
+        expect(o.states.initial === o.current).toBe(true);
+    });
+
+    it('creates property "prior", after first run', function () {
+        expect(typeof o.prior).toBe('undefined');
+        m();
+        expect(typeof o.prior).toBe('function');
+        expect(o.states.initial === o.prior).toBe(true);
+        expect(o.states.final === o.current).toBe(true);
+    });
+
+    it('creates property "machine", a reference to the machine itself', function () {
+        expect(typeof o.machine).toBe('function');
+        expect(m === o.machine).toBe(true);
+    });
+
+});
+
+
+describe('Bad design of machine with callbacks to itself', function () {
+    function request(callback) {
+        callback();
+    }
+
+    var bad = {
+        states: {
+            initial: function () {
+                request(this.machine);
+                return this.states.final;
+            },
+            final: function () {
+                this.done = true;
+                return;
+            }
+        }
+    };
+
+    it('cant avoid race condition', function () {
+        var m = u(bad);
+        expect(function () {
+            m();
+        }).toThrow();
+    });
+
+});
+
+
+describe('Good design of machine with callback to itself', function () {
+    function external(callback) {
+        callback();
+    }
+
+    var good = {
+        states: {
+            initial: function () {
+                var self = this;
+                setTimeout(function () {
+                    external(self.machine);
+                });
+                return this.states.final;
+            },
+            final: function () {
+                this.done = true;
+                return;
+            }
+        }
+    };
+
+    it('avoids race condition with setTimeout', function (done) {
+        u(good)();
+
+        expect(good.current === good.states.final).toBe(true);
+
+        // Make test asynchronous since the machine is asynchronous too
+        setTimeout(function () {
+            expect(good.done).toBe(true);
+            done();
+        });
+    });
+});
+
+
+describe('Best design of machine with callbacks to itself', function () {
+    // This design is better since
+    // it doesn't create functions inside of states.
+
+    function external(callback) {
+        callback();
+    }
+
+    var best = {
+        internal: function () {
+            external(this.machine);
+        },
+        states: {
+            initial: function () {
+                setTimeout(this.internal);
+                return this.states.final;
+            },
+            final: function () {
+                this.done = true;
+                return;
+            }
+        }
+    };
+
+    it('avoids race condition with an asynchronous helper', function (done) {
+        best.internal = best.internal.bind(best);
+        u(best)();
+
+        expect(best.current === best.states.final).toBe(true);
+        // Make test asynchronous since the machine is asynchronous too
+        setTimeout(function () {
+            expect(best.done).toBe(true);
+            done();
+        });
     });
 });
