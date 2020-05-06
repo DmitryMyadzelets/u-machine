fe73b17297cf03ddf4f1e40904cbe94302b4737c
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2020-05-06 22:39:00 +0200

    Make ES2015 style

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
-/*jslint browser: false*/
-'use strict';
-
 // ============================================================================
 // Helpers
 
 // Creates o[Function][name] = kinda function name for each function of the object
 // Returns array with created names
 function deanonymize(o, name) {
-    name = name || 'named';
-    var keys;
+    name = name || 'named'
     if (o) {
-        keys = Object.keys(o);
-        keys.forEach(function (key) {
-            if (typeof o[key] === 'function') {
-                o[key][name] = key;
+        const keys = Object.keys(o)
+        keys.forEach(key => {
+            if (typeof o[key] == 'function') {
+                o[key][name] = key
             }
-        });
+        })
+        return keys
+    } else {
+        return []
     }
-    return keys || [];
 }
-
 
 // ============================================================================
 // Core functions
 
 // Entry function for all input events
 function main() {
-    this.prior = this.current;
-    this.current = this.current.apply(this, arguments) || this.current;
+    this.prior = this.current
+    this.current = this.current.apply(this, arguments) || this.current
     // Call user transition function, if defined
     if (this.transition) {
-        this.transition.apply(this, arguments);
+        this.transition.apply(this, arguments)
     }
 }
 
 
 // Returns the machine object main function. Sets the initial state as current
 function constructor(o) {
-    var f = main.bind(o);
-    o.current = o.states.initial || o.initial();
-    o.machine = f;
-    return f;
+    const f = main.bind(o)
+    o.current = o.states.initial || o.initial()
+    o.machine = f
+    return f
 }
 
 
-constructor.deanonymize = deanonymize;
+constructor.deanonymize = deanonymize
 
-module.exports = constructor;
+module.exports = constructor
